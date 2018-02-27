159741f8789434371903ba99d662fd6317ad73b9
KimDaeyong <01rlaeodyd@naver.com>
2018-02-27 17:50:35 +0900

    Separates Setting page with Page

---
+++ CalendarBackground/View/Pages/Setting/SettingBackgroundPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using BingHelper;
+using CalendarBackground.Model;
+using Microsoft.Win32;
+
+namespace CalendarBackground.View.Pages.Setting
+{
+    /// <summary>
+    /// Interaction logic for SettingBackgroundPage.xaml
+    /// </summary>
+    public partial class SettingBackgroundPage : Page
+    {
+        public SettingBackgroundPage()
+        {
+            InitializeComponent();
+            this.DataContext = App.backgroundViewModel;
+        }
+
+        private void SetPreviewImageSize()
+        {
+            System.Windows.Forms.Screen screen = System.Windows.Forms.Screen.PrimaryScreen;
+            var ratio = screen.WorkingArea.Width / 300;
+            var height = screen.WorkingArea.Height / ratio;
+            rectPreview.Height = height;
+        }
+
+        private void btnPictureSelect_Click(object sender, RoutedEventArgs e)
+        {
+            var filePath = OpenDialog()[0];
+            App.backgroundViewModel.AddBackground(new CBPictureBackground(filePath));
+        }
+
+        private string[] OpenDialog(bool multiSelect = false)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = multiSelect;
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";//이미지 필터링 설정
+            var result = openFileDialog.ShowDialog();
+            if (result ?? false)
+            {
+                return openFileDialog.FileNames;
+            }
+            return null;
+        }
+
+        public async Task SetBingImageAsync()
+        {
+            BingImage bingImage = new BingImage();
+            var bingUnit = await bingImage.GetTodayBingUnit(BingImageResolution.LARGE);
+            if (bingUnit != null)
+            {
+                imgPreview.ImageSource = new BitmapImage(new Uri(bingUnit.Path));
+            }
+        }
+
+        private async void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            var cbxItem = sender as ComboBoxItem;
+            if (cbxItem != null)
+            {
+                var grid = cbxItem.DataContext as Grid;
+                if (grid != null && grid.DataContext != null)
+                {
+                    BackgroundType type = (BackgroundType)grid.DataContext;
+                    if (App.backgroundViewModel.CurrentType != type)
+                    {
+                        await App.backgroundViewModel.ChangeBackgroundByDefaultValueAsync(type);
+                    }
+                    Debug.WriteLine(type);
+                }
+            }
+        }
+
+        private void SettingBackgroundPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SetPreviewImageSize();
+        }
+    }
+}
--- CalendarBackground/View/SettingWindow.xaml.cs
+++ CalendarBackground/View/SettingWindow.xaml.cs
@@ -19,68 +19,6 @@ namespace CalendarBackground.View
         public SettingWindow()
         {
             InitializeComponent();
-
-            this.DataContext = App.backgroundViewModel;
-        }
-
-        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            SetPreviewImageSize();
-        }
-
-        private void SetPreviewImageSize()
-        {
-            System.Windows.Forms.Screen screen = System.Windows.Forms.Screen.PrimaryScreen;
-            var ratio = screen.WorkingArea.Width / 300;
-            var height = screen.WorkingArea.Height / ratio;
-            rectPreview.Height = height;
-        }
-
-        private void btnPictureSelect_Click(object sender, RoutedEventArgs e)
-        {
-            var filePath = OpenDialog()[0];
-            App.backgroundViewModel.AddBackground(new CBPictureBackground(filePath));
-        }
-
-        private string[] OpenDialog(bool multiSelect = false)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Multiselect = multiSelect;
-            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";//이미지 필터링 설정
-            var result = openFileDialog.ShowDialog();
-            if (result ?? false)
-            {
-                return openFileDialog.FileNames;
-            }
-            return null;
-        }
-
-        public async Task SetBingImageAsync()
-        {
-            BingImage bingImage = new BingImage();
-            var bingUnit = await bingImage.GetTodayBingUnit(BingImageResolution.LARGE);
-            if (bingUnit != null)
-            {
-                imgPreview.ImageSource = new BitmapImage(new Uri(bingUnit.Path));
-            }
-        }
-
-        private async void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
-        {
-            var cbxItem = sender as ComboBoxItem;
-            if(cbxItem != null)
-            {
-                var grid = cbxItem.DataContext as Grid;
-                if (grid != null && grid.DataContext != null)
-                {
-                    BackgroundType type = (BackgroundType)grid.DataContext;
-                    if(App.backgroundViewModel.CurrentType != type)
-                    {
-                        await App.backgroundViewModel.ChangeBackgroundByDefaultValueAsync(type);
-                    }
-                    Debug.WriteLine(type);
-                }
-            }
         }
     }
 }
