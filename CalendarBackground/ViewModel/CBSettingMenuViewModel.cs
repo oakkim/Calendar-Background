9039f07409c5e0f61cb407d469c2a1b5464276fb
KimDaeyong <01rlaeodyd@naver.com>
2018-03-01 00:31:11 +0900

    Selected Style Add, Default menu set

--- CalendarBackground/ViewModel/CBSettingMenuViewModel.cs
+++ CalendarBackground/ViewModel/CBSettingMenuViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
@@ -11,11 +13,20 @@ using CalendarBackground.Model;
 
 namespace CalendarBackground.ViewModel
 {
-    public class CBSettingMenuViewModel
+    public class CBSettingMenuViewModel : INotifyPropertyChanged
     {
+        private CBSettingMenu _selectedMenu;
         public ObservableCollection<CBSettingMenu> Items { get; } = new ObservableCollection<CBSettingMenu>();
 
-        public CBSettingMenu SelectedMenu { get; set; }
+        public CBSettingMenu SelectedMenu
+        {
+            get => _selectedMenu;
+            set
+            {
+                _selectedMenu = value;
+                OnPropertyChanged(nameof(SelectedMenu));
+            }
+        }
 
         public CBSettingMenuViewModel()
         {
@@ -26,6 +37,15 @@ namespace CalendarBackground.ViewModel
         {
             Items.Add(new CBSettingMenu(){Idx = 0, Title = "Background", Icon = IconHelper.FindIconCanvasByName("icon_picture") });
             Items.Add(new CBSettingMenu(){Idx = 1, Title = "Calendar", Icon = IconHelper.FindIconCanvasByName("icon_calendar") });
+            SelectedMenu = Items[0];
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
